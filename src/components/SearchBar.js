e0d167797b4d43eb43624c70562053577b880b44
sbroo <seb2g20@soton.ac.uk>
2024-09-02 16:15:22 +0100

    Added filtering of json gifts

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,25 @@
 import React, {useState} from 'react';
 import {FaSearch} from "react-icons/fa";
 import "./SearchBar.css";
+import giftsData from "./gifts.json";
 
 export const SearchBar = () => {
+
     const [input, setInput] = useState('');
+
+    const handleChange = (value) => {
+        setInput(value);
+        const results = giftsData.filter((giftsData) => {
+            return value && giftsData && giftsData.Gift && giftsData.Gift.toLowerCase().includes(value);
+        })
+        console.log(results);
+    }
+
     return <div className="input-wrapper">
     <FaSearch id = "search-icon"></FaSearch>
-    <input placeholder="Type here..." value={input} onChange={(e) => setInput(e.target.value)} />
+    <input placeholder="Type here..."
+           value={input}
+           onChange={(e) => handleChange(e.target.value)}
+    />
     </div>
 }
