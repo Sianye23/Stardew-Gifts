7070fc9ee1166eaa8cb317a670c36b3c82a67012
sbroo <seb2g20@soton.ac.uk>
2024-09-03 17:40:32 +0100

    updated json file

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -1,17 +1,28 @@
-import React, {useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 export const DetailsPage = () => {
-    const location = useLocation();
-    const params = new URLSearchParams(location.search);
+    const information = useLocation().state
 
-    const title = params.get('Gift');
-    const otherInfo = params.get('Loves'); // replace with actual keys
+    const gift = information.Gift;
+
+    function clean(villagers) {
+        return villagers.split('-').join(', ');
+    }
+
+    const love = clean(information.Love);
+    const like = clean(information.Like);
+    const neutral = clean(information.Neutral);
+    const dislike = clean(information.Dislike);
+    const hate = clean(information.Hate);
 
     return (
         <div>
-            <h1>{title}</h1>
-            <p>{otherInfo}</p>
+            <h1>{gift}</h1>
+            <p>Love: {love}</p>
+            <p>Like: {like}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Dislike: {dislike}</p>
+            <p>Hate: {hate}</p>
         </div>
     );
 };
